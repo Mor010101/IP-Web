f06b575914aaaac958ee6d80da8d9ebb41f44c58
Tudor Moroșan <tudor.morosan@student.upt.ro>
2023-04-20 10:01:54 +0300

    added bootstrap

--- IP-Frontend/src/app/app.module.ts
+++ IP-Frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { PacientProfileComponent } from './features/pacient-profile/pacient-profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PacientProfileComponent } from './features/pacient-profile/pacient-prof
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
